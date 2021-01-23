5c3dbf155e2befe41e12d4f43614b78a526be4bf
Rodrigo <rodrigo.nuricumbo1819@uppuebla.edu.mx>
2021-01-23 11:26:30 -0600

    Condicionales

--- Condicionales/src/main/java/Condicionales.java
+++ Condicionales/src/main/java/Condicionales.java
@@ -9,13 +9,8 @@
  * @author Rodrigo
  */
 public class Condicionales {
-<<<<<<< HEAD
     public static void main(String args){
         int Condición1;
         int condición2;
-=======
-    public static void main(String args[]){
-        int condicion;
->>>>>>> 6fcc6c8501a9af5ac5a9f4998b3ce59da5ae15a9
     }
 }
