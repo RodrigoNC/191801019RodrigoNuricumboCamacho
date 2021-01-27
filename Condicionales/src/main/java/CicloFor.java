31ee0054b43094123a80d353b5c40a53bd4dd3d5
Rodrigo <rodrigo.nuricumbo1819@uppuebla.edu.mx>
2021-01-27 08:37:25 -0600

    Condicionales

---
+++ Condicionales/src/main/java/CicloFor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo
+ */
+import java.util.Scanner;
+public class CicloFor {
+    public static void main(String args[])
+    {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Da el número de la serie: ");
+        int serie= scanner.nextInt();
+        for(int i=0; i<=serie; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
