da9bb0c196171564125c4c283d5d4e524c7176c3
Rodrigo <rodrigo.nuricumbo1819@uppuebla.edu.mx>
2021-01-29 08:47:06 -0600

    Castear

---
+++ Castear/src/main/java/Implicito.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Implicito {
+    public static void main(String args[]){
+        int valorMenor=1;
+        double valorMayor=valorMenor;
+        System.out.println(valorMayor);
+    }
+}
