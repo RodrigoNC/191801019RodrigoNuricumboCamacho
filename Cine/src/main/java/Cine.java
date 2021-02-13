31b51fbe171246d1318e2954504429fa20bdc9ea
Rodrigo <rodrigo.nuricumbo1819@uppuebla.edu.mx>
2021-02-13 10:05:52 -0600

    Cine

---
+++ Cine/src/main/java/Cine.java
@@ -0,0 +1,66 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Cine {
+    public String nombreCine;
+    public int horaInicio, horaFin;
+    public ArrayList <Pelicula> peliculas= new ArrayList();
+    
+    public Cine(){
+        this.horaFin= 0;
+        this.horaInicio= 0;
+        this.nombreCine= "";
+        this.peliculas= null;
+    }
+    public Cine(String nombreCine, int horaInicio, int horaFin){
+        this.horaInicio= horaInicio;
+        this.horaFin= horaFin;
+        this.nombreCine= nombreCine;
+        this.peliculas= peliculas;
+    }
+    public String imprimirPeliculas(){
+        String cartelera= "";
+        for(Pelicula pelicula: peliculas){
+            cartelera+= pelicula.titulo+"\n";
+        }
+        return cartelera;
+    }
+    public void agregarPelicula(Pelicula pelicula){
+        peliculas.add(pelicula);
+    }
+    public int cantidadPeliculas(){
+        return peliculas.size();
+    }
+    public String menu(){
+        String menu= "";
+        int contador= 1;
+        menu+= "Menú de películas\n";
+        for(Pelicula pelicula: peliculas){
+            menu+= ""+contador+"-  "+pelicula.titulo+"\n";
+            contador++;
+        }
+        return menu;
+    }
+    public boolean cineAbierto(int hora){
+        if(hora<horaInicio || hora>horaFin){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    @Override
+    public String toString(){
+        return "Cine: "+nombreCine+"\nHora de inicio: "+horaInicio+"\nHora final: "+horaFin;
+    }
+}
--- Cine/src/test/java/TestCine.java
+++ Cine/src/test/java/TestCine.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -11,32 +10,36 @@ import java.util.Scanner;
  *
  * @author Rodrigo
  */
-public class Cine {
+public class TestCine {
     public static void main(String[] args) {
         Scanner scanner= new Scanner (System.in);
         Scanner scan= new Scanner(System.in);
-        int o=0;
-        ArrayList <Pelicula> peliculas = new ArrayList();
+        int opcion=0;
+        int hora=0;
+        Cine cine= new Cine("Cinepolis", 9, 21);
+        System.out.println(cine);
         do{
             System.out.println("¿Quieres agregar una película? ");
             System.out.println("1-Sí\n2-No");
-            o= scan.nextInt();
-            if(o==1){
+            opcion= scan.nextInt();
+            if(opcion==1){
                 System.out.println("Inserta el título de la película: ");
                 String titulo= scanner.nextLine();
                 Pelicula nuevaPelicula= new Pelicula(titulo);
-                peliculas.add(nuevaPelicula);
-                peliculas.size();
+                cine.agregarPelicula(nuevaPelicula);
+                cine.peliculas.size();
             }
             else{
                 System.out.println("Terminado");
                 System.out.println("\n");
             }
         }
-        while(o!=2);
-        System.out.println(peliculas.size());
-        for(Pelicula pelicula: peliculas){
-            System.out.println(pelicula);
-        }
+        while(opcion!=2);
+        System.out.println("Da una hora en formato de 24 hrs: ");
+        hora= scan.nextInt();
+        System.out.println(cine.imprimirPeliculas());
+        System.out.println(cine.menu());
+        System.out.println("La cantidad de películas es "+cine.cantidadPeliculas());
+        System.out.println("¿El cine está abierto? "+cine.cineAbierto(hora));
     }
 }
